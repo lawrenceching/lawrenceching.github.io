7500b14b31a5f5a2d064eecd31d89f40759fd03c
Lawrence Ching <lawrence.ching.zh@gmail.com>
2019-08-30 21:55:03 +0800

    Get Likes from remote server

---
+++ likebutton.js
@@ -0,0 +1,36 @@
+// $(function(){
+//     $(document).one('click', '.like-review', function(e) {
+//         $(this).html('<i class="fa fa-heart" aria-hidden="true"></i> You liked this');
+//         $(this).children('.fa-heart').addClass('animate-like');
+//     });
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    function setCount(count) {
+        document.querySelector("div.like-content button").innerHTML = `<i class="fa fa-heart" aria-hidden="true"></i> <span class="like-count"></span> ${count} Likes`;
+    }
+
+    async function sendLikeToServer() {
+        return fetch('http://like.imlc.me/api/v1/likes', {
+            method: 'POST'
+        });
+    }
+
+
+    fetch('http://like.imlc.me/api/v1/likes')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            const count = json.data;
+            setCount(count);
+
+            document.querySelector("div.like-content button").addEventListener('click', function() {
+                sendLikeToServer();
+                document.querySelector("div.like-content button").innerHTML = `<i class="fa fa-heart" aria-hidden="true"></i> ${count + 1} Likes`;
+                document.querySelector("div.like-content button .fa-heart").classList.add('animate-like');
+            })
+        });
+
+});
